ed67d4f08941871a4209bf813bad40088a839add
Sebastian Montero <smontero@mightywizards.com>
2018-05-17 23:17:01 +0900

    more scripts

--- tensorflow-linear-regression-feature-crosses.py
+++ tensorflow-linear-regression-feature-crosses.py
@@ -43,8 +43,21 @@ def my_input_fn(features, targets, batch_size=1, shuffle=True, num_epochs=None):
     features, labels = ds.make_one_shot_iterator().get_next()
     return features, labels
 
-def construct_feature_columns(input_features):
-    return set([tf.feature_column.numeric_column(feature) for feature in input_features])
+def construct_feature_columns(training_examples):
+    households = tf.feature_column.numeric_column("households")
+    longitude = tf.feature_column.numeric_column("longitude")
+    latitude = tf.feature_column.numeric_column("latitude")
+    housing_median_age = tf.feature_column.numeric_column("housing_median_age")
+    median_income = tf.feature_column.numeric_column("median_income")
+    rooms_per_person = tf.feature_column.numeric_column("rooms_per_person")
+    
+    bucketized_households = tf.feature_column.bucketized_column(households, get_quantile_based_boundry(training_examples['households'], 7))
+    bucketized_longitude = tf.feature_column.bucketized_column(longitude, get_quantile_based_boundry(training_examples['longitude'], 10))
+    bucketized_latitude = tf.feature_column.bucketized_column(latitude, get_quantile_based_boundry(training_examples['latitude'], 10))
+    bucketized_housing_median_age = tf.feature_column.bucketized_column(housing_median_age, get_quantile_based_boundry(training_examples['housing_median_age'], 7))
+    bucketized_median_income = tf.feature_column.bucketized_column(median_income, get_quantile_based_boundry(training_examples['median_income'], 7))
+    bucketized_rooms_per_person = tf.feature_column.bucketized_column(rooms_per_person, get_quantile_based_boundry(training_examples['rooms_per_person'], 5))
+    return set([bucketized_households, bucketized_longitude, bucketized_latitude, bucketized_housing_median_age, bucketized_median_income, bucketized_rooms_per_person])
 
 def predictions_to_numpy_array(predictions):
     return np.array([item['predictions'][0] for item in predictions])
@@ -60,7 +73,7 @@ def train_model(learning_rate, steps, batch_size, training_examples, training_ta
    
     feature_columns = construct_feature_columns(training_examples)
     
-    optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate)
+    optimizer = tf.train.FtrlOptimizer(learning_rate=learning_rate)
     optimizer = tf.contrib.estimator.clip_gradients_by_norm(optimizer, 5.0)
     
     linear_regressor = tf.estimator.LinearRegressor(feature_columns = feature_columns, optimizer=optimizer)
@@ -108,6 +121,11 @@ def select_and_transform_features(source_df):
         selected_examples['lat_range_{}_to_{}'.format(r[0], r[1])] = source_df['latitude'].apply(lambda l: 1.0 if l >= r[0] and l < r[1] else 0.0)
     return selected_examples
     
+def get_quantile_based_boundry(feature_values, num_buckets):
+    boundries = np.arange(1.0, num_buckets) / num_buckets
+    quantiles = feature_values.quantile(boundries)
+    return [quantiles[q] for q in quantiles.keys()]
+    
 
 tf.logging.set_verbosity(tf.logging.ERROR)
 pd.options.display.max_rows = 10
@@ -118,6 +136,8 @@ california_housing_dataframe = california_housing_dataframe.reindex(np.random.pe
 
 training_examples = preprocess_features(california_housing_dataframe.head(12000))
 training_targets = preprocess_targets(california_housing_dataframe.head(12000))
+
+print(training_examples.describe())
 
 validation_examples = preprocess_features(california_housing_dataframe.tail(5000))
 validation_targets = preprocess_targets(california_housing_dataframe.tail(5000))
@@ -135,8 +155,8 @@ print(LATITUDE_RANGES1)'''
 LATITUDE_RANGES = list(zip(range(lat_start, lat_end - 1), range(lat_start + 1, lat_end)))
 #LATITUDE_RANGES = [[32,33]]
 
-selected_training_examples = select_and_transform_features(training_examples)
-selected_validation_examples = select_and_transform_features(validation_examples)
+#selected_training_examples = select_and_transform_features(training_examples)
+#selected_validation_examples = select_and_transform_features(validation_examples)
 
 
 #plt.hist(training_examples['latitude'], 10)
@@ -144,14 +164,14 @@ selected_validation_examples = select_and_transform_features(validation_examples
 
 
 
-linear_regressor = train_model(learning_rate = 0.01, steps=500, batch_size=10, training_examples=selected_training_examples, training_targets=training_targets, validation_examples=selected_validation_examples, validation_targets=validation_targets)
+linear_regressor = train_model(learning_rate = 1.0, steps=500, batch_size=10, training_examples=training_examples, training_targets=training_targets, validation_examples=validation_examples, validation_targets=validation_targets)
 
 california_housing_test_data = pd.read_csv("https://storage.googleapis.com/mledu-datasets/california_housing_test.csv", sep=",")
 
 test_examples = preprocess_features(california_housing_test_data)
 test_targets = preprocess_targets(california_housing_test_data)
-selected_test_examples = select_and_transform_features(test_examples)
-predict_test_input_fn = lambda: my_input_fn(selected_test_examples, test_targets, shuffle=False, num_epochs=1)
+#selected_test_examples = select_and_transform_features(test_examples)
+predict_test_input_fn = lambda: my_input_fn(test_examples, test_targets, shuffle=False, num_epochs=1)
 
 test_predictions = linear_regressor.predict(input_fn=predict_test_input_fn)
 test_predictions = predictions_to_numpy_array(test_predictions)
