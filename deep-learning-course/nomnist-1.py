b89bcb09dbaf76fb25338c83fa7101f7d85c7e1e
Sebastian Montero <smontero@mightywizards.com>
2018-07-10 21:16:13 +0900

    added exercises

--- deep-learning-course/nomnist-1.py
+++ deep-learning-course/nomnist-1.py
@@ -9,6 +9,7 @@ from IPython.display import display, Image
 from sklearn.linear_model import LogisticRegression
 from six.moves.urllib.request import urlretrieve
 from six.moves import cPickle as pickle
+from numpy import dtype
 
 
 url = 'https://commondatastorage.googleapis.com/books1000/'
@@ -116,10 +117,10 @@ def maybe_pickle(data_folders, min_num_images_per_class, force=False):
   
   return dataset_names
 
-def make_arrays(nb_rows, img_size):
+def make_arrays(nb_rows, img_size, num_classes):
   if nb_rows:
     dataset = np.ndarray((nb_rows, img_size ** 2), dtype=np.float32)
-    labels = np.ndarray(nb_rows, dtype=np.int32)
+    labels = np.ndarray((nb_rows, num_classes), dtype=np.float32)
   else:
     dataset, labels = None, None
   return dataset, labels
@@ -127,13 +128,17 @@ def make_arrays(nb_rows, img_size):
 def reshape_image(image_array, num_images):
     return np.reshape(image_array, [num_images, -1])
 
+def reshape_label(labels, num_classes):
+    return np.eye(10, dtype=np.float32)[labels]
+
 def merge_datasets(pickle_files, train_size, valid_size=0):
   num_classes = len(pickle_files)
-  valid_dataset, valid_labels = make_arrays(valid_size, image_size)
-  train_dataset, train_labels = make_arrays(train_size, image_size)
+  valid_dataset, valid_labels = make_arrays(valid_size, image_size, num_classes)
+  train_dataset, train_labels = make_arrays(train_size, image_size, num_classes)
   vsize_per_class = valid_size // num_classes
   tsize_per_class = train_size // num_classes
-    
+   
+  hot_classes = np.eye(num_classes, dtype=np.float32) 
   start_v, start_t = 0, 0
   end_v, end_t = vsize_per_class, tsize_per_class
   end_l = vsize_per_class+tsize_per_class
@@ -146,13 +151,13 @@ def merge_datasets(pickle_files, train_size, valid_size=0):
         if valid_dataset is not None:
           valid_letter = letter_set[:vsize_per_class, :, :]
           valid_dataset[start_v:end_v, :] = reshape_image(valid_letter, vsize_per_class)
-          valid_labels[start_v:end_v] = label
+          valid_labels[start_v:end_v] = hot_classes[label]
           start_v += vsize_per_class
           end_v += vsize_per_class
                     
         train_letter = letter_set[vsize_per_class:end_l, :, :]
         train_dataset[start_t:end_t, :] = reshape_image(train_letter, tsize_per_class)
-        train_labels[start_t:end_t] = label
+        train_labels[start_t:end_t] = hot_classes[label]
         start_t += tsize_per_class
         end_t += tsize_per_class
     except Exception as e:
@@ -163,23 +168,69 @@ def merge_datasets(pickle_files, train_size, valid_size=0):
   
 def randomize(dataset, labels):
   permutation = np.random.permutation(labels.shape[0])
-  shuffled_dataset = dataset[permutation,:,:]
+  shuffled_dataset = dataset[permutation,:]
   shuffled_labels = labels[permutation]
   return shuffled_dataset, shuffled_labels
-  
+    
+def maybe_pickle_final_dataset(train_datasets, test_datasets, train_size, valid_size, test_size,  force=False):
+    
+    pickle_file = os.path.join(data_root, 'notMNIST.pickle')
+
+    if os.path.exists(pickle_file) and not force:
+        print('Final dataset pickle present, loading from pickle')
+        try:
+            with open(pickle_file, 'rb') as f:
+                dataset = pickle.load(f)
+            
+            train_dataset, train_labels = dataset['train_dataset'], dataset['train_labels'] 
+            valid_dataset, valid_labels = dataset['valid_dataset'], dataset['valid_labels']
+            test_dataset, test_labels = dataset['test_dataset'], dataset['test_labels']
+        except Exception as e:
+            print('Unable to read data to', pickle_file, ':', e)
+            raise
+    else:
+        print('Final dataset not present, merging datasets')
+        valid_dataset, valid_labels, train_dataset, train_labels = merge_datasets(
+            train_datasets, train_size, valid_size)
+        _, _, test_dataset, test_labels = merge_datasets(test_datasets, test_size)
+
+        train_dataset, train_labels = randomize(train_dataset, train_labels)
+        test_dataset, test_labels = randomize(test_dataset, test_labels)
+        valid_dataset, valid_labels = randomize(valid_dataset, valid_labels)
+        try:
+            f = open(pickle_file, 'wb')
+            save = {
+                'train_dataset': train_dataset,
+                'train_labels': train_labels,
+                'valid_dataset': valid_dataset,
+                'valid_labels': valid_labels,
+                'test_dataset': test_dataset,
+                'test_labels': test_labels,
+                }
+            pickle.dump(save, f, pickle.HIGHEST_PROTOCOL)
+            f.close()
+        except Exception as e:
+            print('Unable to save data to', pickle_file, ':', e)
+            raise
+    
+    print('Training:', train_dataset.shape, train_labels.shape)
+    print('Validation:', valid_dataset.shape, valid_labels.shape)
+    print('Testing:', test_dataset.shape, test_labels.shape)
+
+    statinfo  = os.stat(pickle_file)
+    print('Compressed pickle size:', statinfo.st_size)
+    
+    return train_dataset, train_labels, valid_dataset, valid_labels, test_dataset, test_labels
+
 def train_linear(train_dataset, train_labels, valid_dataset, valid_labels):
     
-    print(train_labels)
     model = LogisticRegression()
     model.fit(train_dataset, train_labels)
-    print(model.score(valid_dataset, valid_labels))
-            
+    print('Score on valid dataset:', model.score(valid_dataset, valid_labels))
             
 train_size = 200000
 valid_size = 10000
 test_size = 10000
-
-
 
 num_classes = 10
 np.random.seed(133)
@@ -194,39 +245,6 @@ test_folders = maybe_extract(test_filename)
 train_datasets = maybe_pickle(train_folders, 45000)
 test_datasets = maybe_pickle(test_folders, 1800)
 
-valid_dataset, valid_labels, train_dataset, train_labels = merge_datasets(
-  train_datasets, train_size, valid_size)
-_, _, test_dataset, test_labels = merge_datasets(test_datasets, test_size)
-
-train_dataset, train_labels = randomize(train_dataset, train_labels)
-test_dataset, test_labels = randomize(test_dataset, test_labels)
-valid_dataset, valid_labels = randomize(valid_dataset, valid_labels)
-
-print('Training:', train_dataset.shape, train_labels.shape)
-print('Validation:', valid_dataset.shape, valid_labels.shape)
-print('Testing:', test_dataset.shape, test_labels.shape)
-
-
-pickle_file = os.path.join(data_root, 'notMNIST.pickle')
-
-try:
-  f = open(pickle_file, 'wb')
-  save = {
-    'train_dataset': train_dataset,
-    'train_labels': train_labels,
-    'valid_dataset': valid_dataset,
-    'valid_labels': valid_labels,
-    'test_dataset': test_dataset,
-    'test_labels': test_labels,
-    }
-  pickle.dump(save, f, pickle.HIGHEST_PROTOCOL)
-  f.close()
-except Exception as e:
-  print('Unable to save data to', pickle_file, ':', e)
-  raise
-  
-
-statinfo  = os.stat(pickle_file)
-print('Compressed pickle size:', statinfo.st_size)
-
-train_linear(train_dataset, train_labels, valid_dataset, valid_labels)
+train_dataset, train_labels, valid_dataset, valid_labels, test_dataset, test_labels = maybe_pickle_final_dataset(train_datasets, test_datasets, train_size, valid_size, test_size, True)
+print(train_labels)
+#train_linear(train_dataset, train_labels, valid_dataset, valid_labels)
